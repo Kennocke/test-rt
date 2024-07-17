ef8aeda965e62dfce1a7f230f6392d4d04e90ed1
svitkovskiy <Stasvitkovskiy700@gmail.com>
2024-07-17 16:00:50 +0300

    Поправил тест для Groovy

--- src/test/groovy/ru/svitkovskiy/MainGroovyTest.groovy
+++ src/test/groovy/ru/svitkovskiy/MainGroovyTest.groovy
@@ -5,10 +5,11 @@ import spock.lang.Specification
 class MainGroovyTest extends Specification {
     def "Test countElements with #list"() {
         expect:
-        MainGroovy.countElements(list) == expectedMap
+            MainGroovy.countElements(list) == expectedMap
         where:
             list               | expectedMap
             [1, 1, 2, 2, 3, 3] | [1: 2, 2: 2, 3: 2]
+            ['A', 1, 'D', 'D'] | ['A': 1, 1: 1, 'D': 2]
             []                 | [:]
             null               | [:]
     }
